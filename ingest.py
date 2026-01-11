620873ff19a87feeee46ba8152d5707059ad38d5
Alicia Key <2609896+akey7@users.noreply.github.com>
2026-01-11 10:58:10 -0700

    Issue #5
    Prepare database connection in the ingest script.

---
+++ ingest.py
@@ -0,0 +1,18 @@
+import os
+import psycopg2
+from psycopg2 import pool
+from dotenv import load_dotenv
+from src.import_worker import ImportWorker
+
+load_dotenv()
+
+pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port="5432",
+    database=os.getenv("DB_NAME"),
+)
+import_worker = ImportWorker(pool)
