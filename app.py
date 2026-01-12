5c4603b3dffbe5b24b9d4660ea445949f0f6682b
Alicia Key <2609896+akey7@users.noreply.github.com>
2026-01-12 10:40:40 -0700

    Issue #7
    Place initial components of chat app.

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ from huggingface_hub import login
 from sentence_transformers import SentenceTransformer
 from langchain_huggingface import HuggingFaceEmbeddings
 from src.rag_embeddings_workflow import RagEmbeddingsWorkflow
+from src.agent_mixin import AgentMixin
 
 
-class RagChat:
+class RagChat(AgentMixin):
     def __init__(self):
         load_dotenv()
         self.pool = psycopg2.pool.SimpleConnectionPool(
@@ -34,13 +35,46 @@ class RagChat:
             hf_embeddings=hf_embeddings,
         )
         logging.basicConfig(level=logging.INFO)
+        self.citations = []
+        self.model = "gpt-5-mini"
+
+    def generate_status(self):
+        """
+        Generate a string of markdown that has citations for the RAG and
+        the model being used.
+
+        Returns
+        -------
+        str
+            Markdown to render on the UI and save to the chat file.
+        """
+        if not self.citations:
+            return f"""
+# Model{os.linesep}{os.linesep}`{self.model}`
+# Citations{os.linesep}{os.linesep}- Start the chat to see citations.
+""".strip()
+        else:
+            citations_list = f"{os.linesep}1. ".join(self.citations)
+            return f"""
+# Model{os.linesep}{os.linesep}`{self.model}`
+# Citations{os.linesep}{os.linesep}1. {citations_list}.
+""".strip()
 
     def gradio_app(self):
         with gr.Blocks(title="AliciaData RAG") as demo:
             gr.Markdown("# AliciaData RAG")
             with gr.Tabs():
                 with gr.TabItem("Chat"):
-                    gr.Markdown("Coming soon!")
+                    with gr.Row():
+                        with gr.Column():
+                            chatbot = gr.Chatbot()
+                        with gr.Column():
+                            status_message = gr.Markdown(self.generate_status())
+                    with gr.Row():
+                        with gr.Column():
+                            msg = gr.Textbox(label="Prompt (press enter to send)")
+                        with gr.Column():
+                            clar_button = gr.Button("Clear", variant="stop")
                 with gr.TabItem("Embeddings"):
                     gr.Markdown("Coming soon!")
 
